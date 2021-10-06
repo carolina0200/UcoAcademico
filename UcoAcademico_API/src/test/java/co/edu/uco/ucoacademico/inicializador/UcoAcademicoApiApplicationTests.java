2303a082a433a3303048e601625676ca61480c07
Usuario 207 <usuario.207@estudiantes.uco>
2021-10-05 20:23:23 -0500

    Octubre 5 2021 - 1

---
+++ UcoAcademico_API/src/test/java/co/edu/uco/ucoacademico/inicializador/UcoAcademicoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uco.ucoacademico.inicializador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UcoAcademicoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
